cef3f0decce1fdfe88f27b1c6a15e74d9a2b48d0
Killiancorbel <killian.corbel@epitech.eu>
2018-12-13 10:54:54 +0100

    deployment api

--- src/HO/UserBundle/Entity/User.php
+++ src/HO/UserBundle/Entity/User.php
@@ -53,7 +53,7 @@ class User implements UserInterface
     /**
      * @var string
      *
-     * @ORM\Column(name="token", type="string", length=255)
+     * @ORM\Column(name="token", type="string", length=255, nullable=true)
      */
     private $token;
 
